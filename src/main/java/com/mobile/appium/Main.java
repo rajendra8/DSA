8b5e55867576c62a402340a434c3806db770d076
rajendra_sahu <rajendra8.sahu@gmail.com>
2025-02-11 13:08:56 +0530

    adding solition

--- src/main/java/com/mobile/appium/Main.java
+++ src/main/java/com/mobile/appium/Main.java
@@ -3,5 +3,6 @@ package com.mobile.appium;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
     }
 }
--- src/main/java/com/mobile/appium/String.java
+++
@@ -1,4 +0,0 @@
-package com.mobile.appium;
-
-public class String {
-}
